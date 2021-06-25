f4565a438fbf4f09222b71fd479198d195c93197
Ryan Gomes <>
2021-06-25 14:10:46 -0700

    signInPage

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import BarcodeView from './app/screens/BarcodeView';
+import SignInPage from './app/screens/SignInPage';
+
 
 export default function App() {
-  return <BarcodeView />;
+  return (
+  <SignInPage />
+  );
+
 }
 
 const styles = StyleSheet.create({

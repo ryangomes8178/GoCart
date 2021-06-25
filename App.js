df5a3a98281057879f2f36da499762cce660549e
Vishal Vinjapuri <vishal.vinjapuri@berkeley.edu>
2021-06-24 23:21:31 -0700

    added barcode scanning

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BarcodeView from './app/screens/BarcodeView';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hey bastards!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <BarcodeView />;
 }
 
 const styles = StyleSheet.create({
